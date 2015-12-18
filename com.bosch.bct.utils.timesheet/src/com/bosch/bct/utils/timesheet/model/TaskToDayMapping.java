10c0367fd8f14cb0399d0757f5f725828188ab97
Prashanth Parameshwara <prashanth.parameshwara@in.bosch.com>
2015-12-18 17:34:49 +0530

    Merged with latest UI branch

---
+++ com.bosch.bct.utils.timesheet/src/com/bosch/bct/utils/timesheet/model/TaskToDayMapping.java
@@ -0,0 +1,59 @@
+package com.bosch.bct.utils.timesheet.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TaskToDayMapping {
+
+	private Map<Task, Double> mondayTasks = new HashMap<>();
+	private Map<Task, Double> tuesdayTasks = new HashMap<>();
+	private Map<Task, Double> wednesTasks = new HashMap<>();
+	private Map<Task, Double> thursdayTasks = new HashMap<>();
+	private Map<Task, Double> fridayTasks = new HashMap<>();
+	
+	public void addTaskToMonday(Task task, double effort) {
+		assert task != null;
+		mondayTasks.put(task, effort);
+	}
+	
+	public void addTaskToTuesday(Task task, double effort) {
+		assert task != null;
+		tuesdayTasks.put(task, effort);
+	}
+	
+	public void addTaskToWednesday(Task task, double effort) {
+		assert task != null;
+		wednesTasks.put(task, effort);
+	}
+	
+	public void addTaskToThursday(Task task, double effort) {
+		assert task != null;
+		thursdayTasks.put(task, effort);
+	}
+	
+	public void addTaskToFriday(Task task, double effort) {
+		assert task != null;
+		fridayTasks.put(task, effort);
+	}
+	
+	public Map<Task, Double> getMondayTasks() {
+		return mondayTasks;
+	}
+
+	public Map<Task, Double> getTuesdayTasks() {
+		return tuesdayTasks;
+	}
+
+	public Map<Task, Double> getWednesTasks() {
+		return wednesTasks;
+	}
+
+	public Map<Task, Double> getThursdayTasks() {
+		return thursdayTasks;
+	}
+
+	public Map<Task, Double> getFridayTasks() {
+		return fridayTasks;
+	}
+	
+}
